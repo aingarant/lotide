9ae7bca50c9c59663a69c3395d59243239ca9344
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-15 20:49:34 -0500

    Completed eqArray.js

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -31,6 +31,3 @@ const eqArrays = (array1, array2) => {
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
