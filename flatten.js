0dce5bec868ca40a1bc37e185863b290656805a6
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-21 18:43:12 -0500

    updated flatten

--- flatten.js
+++ flatten.js
@@ -1,5 +1,18 @@
-const flatten = function (input)
-{
-  
+const flatten = function (input) {
+  let output = [];
+
+
+  if (Array.isArray(input)) {
+    input.forEach(item => {
+      if (Array.isArray(item)) {
+        item.forEach(subItem => {
+          output.push(subItem);
+        });
+      } else {
+        output.push(item);
+      }
+    });
+  }
+  return output;
 }
-console.log(flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6])
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6])
