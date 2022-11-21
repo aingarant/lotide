2abd6c3dc95f7898252a12e9a053bec0d17c3d42
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-21 12:29:20 -0500

    finished middletest

--- middle.js
+++ middle.js
@@ -1,28 +1,7 @@
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) return false;
-
-  for (let index in array1) {
-    if (array1[index] !== array2[index]) return false;
-  }
-
-  return true;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  let response = "";
-
-  if (eqArrays(actual, expected) === true) {
-    response = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    response = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-  return response;
-};
-
 const middle = (array) => {
   if (array.length < 3) return [];
   if (array.length % 2 !== 0) return array[Math.floor(array.length / 2)];
   if (array.length % 2 === 0) return [array[Math.floor(array.length / 2) - 1], array[Math.floor(array.length / 2)]];
 };
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [4,5] for [1,2,3,4,5,6,7,8]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+  });
+
+  it("returns ['3'] for ['1,2,3,4,5']", () => {
+    assert.strictEqual(middle(['1','2','3','4','5']), '3');
+  });
+});
