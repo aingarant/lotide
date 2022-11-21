b6c76c4f31f459dc934c16935c9b849015c9bf06
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-21 10:06:20 -0500

    moved test into test folder

--- head.js
+++ head.js
@@ -1,22 +1,10 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected) => {
-  let response = "";
-
-  if (actual === expected) {
-    response = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    response = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-  return response;
-};
+const assertEqual = require('./test/assertEqualTest');
 
 const head = (inputArray) => {
   return inputArray[0] ? inputArray[0] : undefined;
-}
+};
 
 
-
-// TEST CODE
 console.log(assertEqual(head([5, 6, 7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
 console.log(assertEqual(head([]), 5));
