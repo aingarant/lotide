79b44770a2dbc718de137ac420d5c8b0456f1fb7
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-21 10:08:05 -0500

    updated tail

--- tail.js
+++ tail.js
@@ -1,26 +1,11 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = (actual, expected) => {
-  let response = "";
-
-  if (actual === expected) {
-    response = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    response = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-  return response;
-};
+const assertEqual = require('./test/assertEqualTest');
 
 const tail = (inputArray) => {
   return inputArray.length > 2 ? inputArray.slice(1) : [];
 };
 
-// TEST CODE
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// console.log(assertEqual(JSON.stringify(result), JSON.stringify(["Lighthouse", "Labs"])))
-
-
-const words = [];
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const words = [];
+const words = ["Yo Yo", "Lighthouse", "Labs"];
 // const words = ["Labs"];
 
 tail(words); // no need to capture the return value since we are not checking it
